b254bccc665d6faa5cea38e872278c285b8d6e9d
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-05-01 15:20:17 +0200

    Added migration, updated database for feedback

--- BethanysPieShop/BethanysPieShop/Migrations/20180501131617_Feedback added.Designer.cs
+++ BethanysPieShop/BethanysPieShop/Migrations/20180501131617_Feedback added.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BethanysPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180501131617_Feedback added")]
+    partial class Feedbackadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
