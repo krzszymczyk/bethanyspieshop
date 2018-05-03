6793fe6989fd5b7036ba56c60e67a4f0be8f2f14
Krzysztof <szymon7907@gmail.com>
2018-05-03 10:48:11 +0200

    * UserPrefs.xml: Course completed
    * BethanysPieShop.csproj:
    * project.assets.json:
    * Login.cshtml:
    * _Layout.cshtml:
    * LoginViewModel.cs:
    * Register.cshtml:
    * AccountController.cs:
    * FeedbackController.cs:
    * BethanysPieShop.csproj.nuget.cache:
    * BethanysPieShop.csproj.nuget.g.props:
    * BethanysPieShop.csproj.nuget.g.targets:
    * BethanysPieShop.dll:
    * BethanysPieShop.pdb:
    * BethanysPieShop.dll:
    * BethanysPieShop.pdb:
    * BethanysPieShop.deps.json:
    * BethanysPieShop.runtimeconfig.dev.json:
    * BethanysPieShop.csproj.FileListAbsolute.txt:
    * BethanysPieShop.csproj.CoreCompileInputs.cache:
    * BethanysPieShop.csprojResolveAssemblyReference.cache:

---
+++ BethanysPieShop/BethanysPieShop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
